2f60fb68975d8349ce85ce68a8f84591e6a61cc2
Waris Agung Widodo <ido.alit@gmail.com>
2022-07-07 12:55:40 +0700

    Update method to run parser

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pick import pick
 from time import sleep
-from parser.INAJOG import INAJOG as inajog
+from parser.INAJOG import INAJOG
 from parser.kespro import KesPro
 from parser.jurkeb import JurKep
 from parser.jkp import JKP
@@ -8,17 +8,19 @@ from parser.jri import JRI
 from parser.radiationoncology import RadiationOncology
 
 def main():
+    parsers = {
+        "Journal of Reproduction & Infertility": (JRI, "https://www.jri.ir/en/archive"),
+        "Radiation Oncology": (RadiationOncology, "https://link.springer.com/journal/13014/volumes-and-issues"),
+        "INAJOG": (INAJOG, "http://www.inajog.com/index.php/journal/issue/archive"),
+        "Jurnal Kesehatan Reproduksi": (KesPro, "https://ejournal2.litbang.kemkes.go.id/index.php/kespro/issue/archive"),
+        "Jurnal Kebidanan": (JurKep, "https://ejournal.poltekkes-smg.ac.id/ojs/index.php/jurkeb/issue/archive"),
+        "Jurnal Ners dan Kebidanan": (JurKep, "http://jnk.phb.ac.id/index.php/jnk/issue/archive"),
+        "Jurnal Keperawatan Pajajaran": (JKP, "http://jkp.fkep.unpad.ac.id/index.php/jkp/issue/archive"),
+    }
+
     title = "Please select journal:"
-    options = [
-            "Journal of Reproduction & Infertility", #0
-            "Radiation Oncology", #1
-            "INAJOG", #2
-            "Jurnal Kesehatan Reproduksi", #3
-            "Jurnal Kebidanan", #4
-            "Jurnal Ners dan Kebidanan", #5
-            "Jurnal Keperawatan Pajajaran", #6
-            "⛔️ quit"
-        ]
+    options = [p for p in sorted(parsers.keys())]
+    options.append("⛔️ quit")
 
     selected, index = pick(options, title)
 
@@ -26,20 +28,9 @@ def main():
         print("Bye!")
         exit()
 
-    if index == 0:
-        JRI(selected, "https://www.jri.ir/en/archive")
-    elif index == 1:
-        RadiationOncology(selected, "https://link.springer.com/journal/13014/volumes-and-issues")
-    elif index == 2:
-        inajog(selected, "http://www.inajog.com/index.php/journal/issue/archive")
-    elif index == 3:
-        KesPro(selected, "https://ejournal2.litbang.kemkes.go.id/index.php/kespro/issue/archive")
-    elif index == 4:
-        JurKep(selected, "https://ejournal.poltekkes-smg.ac.id/ojs/index.php/jurkeb/issue/archive")
-    elif index == 5:
-        JurKep(selected, "http://jnk.phb.ac.id/index.php/jnk/issue/archive")
-    elif index == 6:
-        JKP(selected, "http://jkp.fkep.unpad.ac.id/index.php/jkp/issue/archive")
+    # run parser
+    cls, url = parsers[selected]
+    cls(selected, url)
 
     # reselect journal
     sleep(2)
